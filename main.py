ffed6d768076ade769533984fdc729d9c8ae7d62
Thomas McCall <thomas.e.mccall@gmail.com>
2020-11-23 12:15:38 -0800

    time the operations

--- main.py
+++ main.py
@@ -1,25 +1,28 @@
 from itertools import product
 import enchant
+import time
 
 # define our spell-checking dictionary
 d = enchant.Dict("en_US")
 
 # letters from the game
-#letters = list("aoputcn")
-letters = list("cpanyoh")
+letters = list("aoputcn")
+
 # necessary letter all words must contain
-#center_letter = "n"
-center_letter = "y"
+center_letter = "n"
+
 all_words = []
 
 print("Analyzing generated pseudo-words...")
-for i in range(4, 8):
+for i in range(4, 10):
     print("---> Target: {}-letter words".format(i))
+    start = time.time()
     for c in product(letters, repeat = i):
         word = ''.join(c)
-        # print(word)
         if center_letter in word and d.check(word):
             all_words.append(word)
+    stop = time.time()
+    print("Analysis time: {}".format(stop - start))
 
 print("Found {} words: {}".format(len(all_words), all_words))
 
@@ -30,16 +33,8 @@ for w in all_words:
         print("Found pandrome: {}".format(w))
         pandromes.append(w)
 
-# If we didn't find a pandrome, try a longer combination. After 7 letters, the
-# time it takes to generate the certesian product increases substantially
+# If we didn't find a pandrome, we could try a longer combination.
+# After 7 letters, though, the time it takes to check all of the certesian
+# products increases substantially, so I just stop here.
 if len(pandromes) == 0:
-    print("Pandrome is longer than 7 letters. Generating 8-letter words...")
-    print("This will take some time.")
-
-c_8 = product(letters, repeat = 8)
-for c in c_8:
-    word = ''.join(c)
-    # print(word)
-    if center_letter in word and d.check(word):
-        print("Found an 8-letter word: {}".format(word))
-        all_words.append(word)
+    print("Pandrome is longer than 9 letters. I don't check that many. Sorry :(")
