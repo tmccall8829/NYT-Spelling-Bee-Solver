508cc9dbb516f40232ce4343984d6c9626c90f15
Thomas McCall <thomas.e.mccall@gmail.com>
2020-11-23 12:07:11 -0800

    check all combos between 4 and 7 letters in a loop

--- main.py
+++ main.py
@@ -5,48 +5,21 @@ import enchant
 d = enchant.Dict("en_US")
 
 # letters from the game
-letters = list("aoputcn")
-
+#letters = list("aoputcn")
+letters = list("cpanyoh")
 # necessary letter all words must contain
-center_letter = "n"
-
-print("Generating combinations...")
-# most of the words will be between 4 and 7 letters long
-c_4 = product(letters, repeat = 4)
-c_5 = product(letters, repeat = 5)
-c_6 = product(letters, repeat = 6)
-c_7 = product(letters, repeat = 7)
-
+#center_letter = "n"
+center_letter = "y"
 all_words = []
 
 print("Analyzing generated pseudo-words...")
-print("Target: four-letter words...")
-for c in c_4:
-    word = ''.join(c)
-    # print(word)
-    if center_letter in word and d.check(word):
-        all_words.append(word)
-
-print("Target: five-letter words...")
-for c in c_5:
-    word = ''.join(c)
-    # print(word)
-    if center_letter in word and d.check(word):
-        all_words.append(word)
-
-print("Target: six-letter words...")
-for c in c_6:
-    word = ''.join(c)
-    # print(word)
-    if center_letter in word and d.check(word):
-        all_words.append(word)
-
-print("Target: seven-letter words...")
-for c in c_7:
-    word = ''.join(c)
-    # print(word)
-    if center_letter in word and d.check(word):
-        all_words.append(word)
+for i in range(4, 8):
+    print("---> Target: {}-letter words".format(i))
+    for c in product(letters, repeat = i):
+        word = ''.join(c)
+        # print(word)
+        if center_letter in word and d.check(word):
+            all_words.append(word)
 
 print("Found {} words: {}".format(len(all_words), all_words))
 
@@ -60,7 +33,8 @@ for w in all_words:
 # If we didn't find a pandrome, try a longer combination. After 7 letters, the
 # time it takes to generate the certesian product increases substantially
 if len(pandromes) == 0:
-    print("Pandrome is longer than 7 letters. Generating 8...")
+    print("Pandrome is longer than 7 letters. Generating 8-letter words...")
+    print("This will take some time.")
 
 c_8 = product(letters, repeat = 8)
 for c in c_8:
