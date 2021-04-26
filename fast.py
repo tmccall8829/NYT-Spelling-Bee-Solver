211c51516731979e59de3a00d210bf25afc7e6b7
Tom McCall <thomas.e.mccall@gmail.com>
2021-04-26 11:18:56 -0500

    added some comments from working copy

--- fast.py
+++ fast.py
@@ -1,17 +1,20 @@
 import time
 import enchant
 
+# use the built-in system dictionary on unix systems
 d = open("/usr/share/dict/words", "r").read().splitlines()
 d_verify = enchant.Dict("en_US")
 
 print("Enter all of the prompts using lowercase letters.")
 print("Make sure to enter today's letters as a single string.")
 
+# type checking is for nerds
 letters = input("What are today's letters? ")
 center_letter = input("What is the center letter? ")
 
 matches = list()
 
+# time our algorithm
 start = time.time()
 for word in d:
     if len(list(word)) >= 4 and center_letter in list(word):
